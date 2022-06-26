5ca7d760462b265c1ef57519eb572048aaa39c27
mpoapostolis <mpoapostolis@gmail.com>
2022-06-26 20:46:39 +0300

    uploadte triirirs models

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div></div>;
+import type { NextApiResponse } from "next";
+
+export default function handler(res: NextApiResponse) {
+  res
+    .status(200)
+    .json({ url: "https://tracker.appoploo.com/salamina/salamina.data" });
 }
--- pages/triiris.tsx
+++ pages/triiris.tsx
@@ -2,18 +2,34 @@ import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function Page() {
+  const x = async () => {
+    return await fetch("/api")
+      .then((data) => data.json())
+      .then((e) => e);
+  };
   const obj = useUnityContext({
-    loaderUrl: "/unity/triiris/salamina.loader.js",
-    dataUrl: "/unity/triiris/salamina.data",
-    frameworkUrl: "/unity/triiris/salamina.framework.js",
-    codeUrl: "/unity/triiris/salamina.wasm",
+    loaderUrl: "/unity/salamina.loader.js",
+    dataUrl:
+      "https://dl-nd365nkd.swisstransfer.com/api/download/3cd9ac0e-0360-4ae9-9c78-782d809972e0/032510f8-0a84-4619-8921-c6593a0e628a?",
+    frameworkUrl: "/unity/salamina.framework.js",
+    codeUrl: "/unity/salamina.wasm",
   });
+  console.log(obj);
   return (
     <div className="bg-black ">
       {!obj.isLoaded && (
-        <div className="flex w-screen h-screen fixed z-50 pointer-events-none justify-center items-center text-white">
-          <div className="animate-spin w-10 h-10"></div>
-          {(obj.loadingProgression * 100).toFixed(0)}%
+        <div className="flex  w-screen h-screen fixed z-50 pointer-events-none justify-center items-center text-white">
+          <div>
+            <div className="text-2xl text-center w-full mb-4">
+              {(obj.loadingProgression * 100).toFixed(2)}%
+            </div>
+            <div className="w-96 bg-gray-200  rounded-full">
+              <div
+                className="bg-blue-600 w-full h-4 text-center text-lg font-medium text-blue-100 p-0.5 leading-none rounded-l-full"
+                style={{ width: `${obj.loadingProgression * 100}%` }}
+              ></div>
+            </div>
+          </div>
         </div>
       )}{" "}
       <Unity className="w-screen h-screen" unityProvider={obj.unityProvider} />

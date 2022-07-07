eecf483847fc1fc27cb821efda26b8653b534de6
mpoapostolis <mpoapostolis@gmail.com>
2022-07-08 02:34:20 +0300

    iframe fix  http error

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         source: "/bucket/:path*",
         destination: "https://appoploo.fra1.digitaloceanspaces.com/:path*",
       },
+      {
+        source: "/iframe/:path*",
+        destination: "http://automatix.talent.gr:82/:path*",
+      },
     ];
   },
 };
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,8 +1,3 @@
 export default function Map() {
-  return (
-    <iframe
-      className="w-screen h-screen"
-      src="http://automatix.talent.gr:82/AO3D_devel/el/"
-    />
-  );
+  return <iframe className="w-screen h-screen" src="/iframe/AO3D_devel/el/" />;
 }

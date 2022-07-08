bfba5bd720aabcfb2a09c1e288735e7039a43340
mpoapostolis <mpoapostolis@gmail.com>
2022-07-08 13:17:31 +0300

    change iframe url

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
         source: "/bucket/:path*",
         destination: "https://appoploo.fra1.digitaloceanspaces.com/:path*",
       },
-      {
-        source: "/iframe/:path*",
-        destination: "http://automatix.talent.gr:82/:path*",
-      },
     ];
   },
 };
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,3 +1,8 @@
 export default function Map() {
-  return <iframe className="w-screen h-screen" src="/iframe/AO3D_devel/el/" />;
+  return (
+    <iframe
+      className="w-screen h-screen"
+      src="https://automatix.talent.gr/AO3D_devel/el/"
+    />
+  );
 }

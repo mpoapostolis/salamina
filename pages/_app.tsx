e88f5fb032def040241041a88070909dd31b4209
mpoapostolis <mpoapostolis@gmail.com>
2022-09-08 13:19:11 +0300

    add fps-stats

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import Link from "next/link";
+// @ts-ignore
+import FPSStats from "react-fps-stats";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
     <main className="relative">
+      <FPSStats />
       {router.pathname !== "/" && (
         <Link href="/">
-          <a className="z-50 fixed text-white top-4 left-4">
+          <a className="z-50 fixed text-white top-16 left-4">
             <svg
               width="24"
               height="24"

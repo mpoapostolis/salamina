6dd77454048e2ed464acf1bdf9c772004456d444
mpoapostolis <mpoapostolis@gmail.com>
2022-10-07 16:02:23 +0300

    hard refresh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,21 +8,24 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className="relative">
       {router.pathname !== "/" && (
-        <Link href="/" passHref>
-          <a className="z-50 fixed text-white top-16 left-4">
-            <svg
-              width="24"
-              height="24"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              color="#FFF"
-            >
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
-            </svg>
-          </a>
-        </Link>
+        <button
+          onClick={() => {
+            window.history.back();
+          }}
+          className="z-50 fixed text-white top-16 left-4"
+        >
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            color="#FFF"
+          >
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+          </svg>
+        </button>
       )}
       <Component {...pageProps} />
     </main>

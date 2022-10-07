031f94d71ada7dd7603818a97b650c2b7242dbe5
mpoapostolis <mpoapostolis@gmail.com>
2022-10-07 13:42:02 +0300

    href

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,21 +11,19 @@ function MyApp({ Component, pageProps }: AppProps) {
     <main className="relative">
       <FPSStats />
       {router.pathname !== "/" && (
-        <Link href="/">
-          <a className="z-50 fixed text-white top-16 left-4">
-            <svg
-              width="24"
-              height="24"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              color="#FFF"
-            >
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
-            </svg>
-          </a>
-        </Link>
+        <a href="/" className="z-50 fixed text-white top-16 left-4">
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            color="#FFF"
+          >
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+          </svg>
+        </a>
       )}
       <Component {...pageProps} />
     </main>
